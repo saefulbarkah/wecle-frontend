9796cc6109065f6a156ec414ca29f9ecac91d264
Saeful Barkah <saefulbarkah650@gmail.com>
2023-11-03 21:35:48 +0700

    feat(model): Add blog model

---
+++ server/src/models/blog.ts
@@ -0,0 +1,25 @@
+import mongoose, { InferSchemaType, Schema } from 'mongoose';
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+type Blog = InferSchemaType<typeof blogSchema>;
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+export default Blog;
