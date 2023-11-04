36983418129527d54e0e22c837378ffd46d45224
Saeful Barkah <saefulbarkah650@gmail.com>
2023-11-04 07:48:11 +0700

    feat(collection): add clear all collection

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { DB_URL } from './config/db.js';
 import * as route from './routes/index.js';
+import User from './models/user.js';
+import Author from './models/author.js';
 
 const app = express();
 const port = 4000;
@@ -11,6 +13,13 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 
 app.get('/', (req, res) => {
   res.send('express + typescript');
+});
+
+// clear collection
+app.get('/clear', async (req, res) => {
+  await User.deleteMany();
+  await Author.deleteMany();
+  res.send('cleared all collection');
 });
 
 app.use('/auth', route.auth);
