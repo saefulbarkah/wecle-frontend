47d49510a589a82c709d1aa44f09da8bde70be13
Saeful Barkah <saefulbarkah650@gmail.com>
2023-11-06 13:18:38 +0700

    feat(author): add update author

--- server/src/models/author.ts
+++ server/src/models/author.ts
@@ -6,6 +6,7 @@ const authorSchema = new Schema({
     ref: 'User',
   },
   name: String,
+  about: String,
   createdAt: {
     type: Date,
     default: Date.now,
---
+++ server/src/schema/auhtor-schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const authorSchema = z
+  .object({
+    _id: z.string({ required_error: 'id is required' }),
+    name: z.string({ required_error: 'name is required' }),
+    about: z.string({ required_error: 'about is required' }),
+  })
+  .required();
+
+type authorType = z.infer<typeof authorSchema>;
+
+export { authorType, authorSchema };
