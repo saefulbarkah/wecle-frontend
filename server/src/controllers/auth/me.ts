b2a8e0f14885d9003d6cef9ccb84c89b4b3fd2c0
Saeful Barkah <saefulbarkah650@gmail.com>
2023-11-14 10:53:55 +0700

    fix: validation decode token on me auth

--- server/src/controllers/auth/me.ts
+++ server/src/controllers/auth/me.ts
@@ -1,17 +1,32 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { ApiResponse } from '../../types/index.js';
+import { UnauthorizedError, ValidationError } from '../../errors/index.js';
+import errorHandling from '../../lib/error-handling.js';
+import User from '../../models/user.js';
 
 const Me = async (req: Request, res: Response) => {
-  if (!req.cookies.auth) return res.json(null);
-  const decode = jwt.verify(req.cookies.auth, process.env.SECRET_JWT as string);
-  const response: ApiResponse = {
-    status: 200,
-    message: 'Opration success',
-    response: 'success',
-    data: decode,
-  };
-  res.status(response.status).json(response);
+  try {
+    if (!req.cookies.auth) throw new UnauthorizedError('Required token');
+    const decode = jwt.verify(
+      req.cookies.auth,
+      process.env.SECRET_JWT as string
+    );
+    const { id } = decode as { id: string };
+    const user = User.findOne({ _id: id });
+    if (!user) throw new ValidationError('Invalid Token');
+
+    const response: ApiResponse = {
+      status: 200,
+      message: 'Opration success',
+      response: 'success',
+      data: user,
+    };
+
+    res.status(response.status).json(response);
+  } catch (error) {
+    errorHandling(error as Error, res);
+  }
 };
 
 export default Me;
