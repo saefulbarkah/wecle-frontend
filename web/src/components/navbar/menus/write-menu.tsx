32a05d314452e08957818e31562c8e77b5fb98f5
saefulbarkah <saefulbarkah650@gmail.com>
2023-11-21 11:00:22 +0700

    refactor(write-menu): change button to Link element

--- web/src/components/navbar/menus/write-menu.tsx
+++ web/src/components/navbar/menus/write-menu.tsx
@@ -4,19 +4,21 @@ import React from 'react';
 import { PenSquare } from 'lucide-react';
 import { SessionType } from '@/hooks/sessions/type';
 import { useAuthOverlay } from '@/features/auth/store';
+import Link from 'next/link';
 
 export const WriteMenu = ({ session }: { session: SessionType }) => {
   const setOverlayAuth = useAuthOverlay((state) => state.setOpen);
 
   return (
-    <button
+    <Link
+      href={`${session ? '/article/new' : '#login'}`}
       className="hidden lg:flex items-center gap-2 text-secondary hover:text-black transition hover:bg-secondary-50/80 rounded-md p-2 "
       onClick={() => {
-        if (!session) setOverlayAuth(true);
+        if (!session) return setOverlayAuth(true);
       }}
     >
       <PenSquare className="w-6 h-6" />
       <span className="text-sm font-semibold">Write</span>
-    </button>
+    </Link>
   );
 };
