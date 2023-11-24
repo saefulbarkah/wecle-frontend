1a2eb948dcd7cf8d85ea5189d54a84ac9e09461f
Saeful Barkah <saefulbarkah650@gmail.com>
2023-11-24 14:22:10 +0700

    feat(editor-create-article): add validation image upload on editor

--- web/src/features/article/create/components/Editor/menus/floating-menu.tsx
+++ web/src/features/article/create/components/Editor/menus/floating-menu.tsx
@@ -3,8 +3,10 @@ import { Button } from '@/components/ui/button';
 import { Editor, FloatingMenu as Menu } from '@tiptap/react';
 import { ImagePlus, LucidePlus } from 'lucide-react';
 import React, { useState } from 'react';
-import { AnimatePresence, Variants, motion } from 'framer-motion';
-import { nanoid } from 'nanoid';
+import { AnimatePresence, motion } from 'framer-motion';
+import { fromZodError } from 'zod-validation-error';
+import { ZodError, z } from 'zod';
+import toast from 'react-hot-toast';
 
 type floatingProps = {
   editor: Editor | null;
@@ -40,7 +42,7 @@ export const FloatingMenu = ({ editor }: floatingProps) => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0, cursor: 'none', pointerEvents: 'none' }}
-                className="absolute -right-6 translate-x-full "
+                className="absolute -right-6 translate-x-full"
               >
                 <div className="min-w-[400px] bg-white">
                   <div className="flex items-center gap-3">
@@ -56,40 +58,73 @@ export const FloatingMenu = ({ editor }: floatingProps) => {
   );
 };
 
+// upload image component
 type uploadImageProps = React.PropsWithChildren & floatingProps & {};
+
+const MAX_FILE_SIZE = 50000;
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+];
+
+const uploadImageSchema = z
+  .custom<File>()
+  .refine((file) => file.size >= MAX_FILE_SIZE, {
+    message: 'Max file size is 5MB',
+  })
+  .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
+    message: '.jpg, .jpeg, .png and .webp files are accepted.',
+  });
+
 const UploadImage = ({ editor }: uploadImageProps) => {
   const fileRef = React.useRef<HTMLInputElement | null>(null);
 
-  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
-    const fileName = file.name.split('.')[0];
-    const img = new Image();
-    img.src = URL.createObjectURL(file);
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = img.width;
-      canvas.height = img.height;
+    try {
+      await uploadImageSchema.parseAsync(file);
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d');
 
-      ctx?.drawImage(img, 0, 0);
+        ctx?.drawImage(img, 0, 0);
 
-      canvas.toBlob(
-        (blob) => {
-          if (!blob) return;
-          const reader = new FileReader();
-          reader.readAsDataURL(blob);
+        canvas.toBlob(
+          (blob) => {
+            if (!blob) return;
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
 
-          reader.onload = (e: ProgressEvent<FileReader>) => {
-            const result = reader.result as string;
-            editor?.chain().focus().setImage({ src: result }).run();
-          };
-        },
-        'image/jpeg',
-        0.5
-      );
-    };
+            reader.onload = (e: ProgressEvent<FileReader>) => {
+              const result = reader.result as string;
+              editor?.chain().focus().setImage({ src: result }).run();
+            };
+          },
+          'image/jpeg',
+          0.5
+        );
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        toast.error(
+          <>
+            <ul className="list-disc ml-5">
+              {error.errors.map((item, i) => (
+                <li key={i}>{item.message}</li>
+              ))}
+            </ul>
+          </>
+        );
+      }
+    }
   };
 
   return (
