f5724453c190dd623a8749f65d6f509369f22cfc
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-01 12:34:25 +0700

    fix(write-navbar): fix refresh page if user trigger click and  not logged in

--- web/src/components/navbar/menus/write-menu.tsx
+++ web/src/components/navbar/menus/write-menu.tsx
@@ -5,20 +5,41 @@ import { PenSquare } from 'lucide-react';
 import { SessionType } from '@/hooks/sessions/type';
 import { useAuthOverlay } from '@/features/auth/store';
 import Link from 'next/link';
+import { cva } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
 
-export const WriteMenu = ({ session }: { session: SessionType }) => {
-  const setOverlayAuth = useAuthOverlay((state) => state.setOpen);
+const classVariant = cva([
+  'hidden lg:flex items-center gap-2 text-secondary hover:text-black transition hover:bg-secondary-50/80 rounded-md p-2',
+]);
 
+const NavigateCreateArticle = () => {
   return (
-    <Link
-      href={`${session ? 'article/new' : '#login'}`}
-      className="hidden lg:flex items-center gap-2 text-secondary hover:text-black transition hover:bg-secondary-50/80 rounded-md p-2 "
+    <Link href={'article/new'} className={cn(classVariant())}>
+      <PenSquare className="w-6 h-6" />
+      <span className="text-sm font-semibold">Write</span>
+    </Link>
+  );
+};
+
+const TriggerLogin = ({ session }: { session: SessionType }) => {
+  const setOverlayAuth = useAuthOverlay((state) => state.setOpen);
+  return (
+    <button
+      className={cn(classVariant())}
       onClick={() => {
         if (!session) return setOverlayAuth(true);
       }}
     >
       <PenSquare className="w-6 h-6" />
       <span className="text-sm font-semibold">Write</span>
-    </Link>
+    </button>
+  );
+};
+
+export const WriteMenu = ({ session }: { session: SessionType }) => {
+  return session ? (
+    <NavigateCreateArticle />
+  ) : (
+    <TriggerLogin session={session} />
   );
 };
