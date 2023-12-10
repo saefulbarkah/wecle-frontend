12ab4c1ea578db3151acec01ca916a7a390ed188
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-10 08:45:21 +0700

    Revert "configure socket io"
    This reverts commit d062d0d14a5dbe015629f7fd7f611de4107d0c8b.

--- server/src/sockets/notification-socket.ts
+++
@@ -1,7 +0,0 @@
-import { Server, Socket } from 'socket.io';
-
-const notificationSocket = (io: Server, socket: Socket) => {
-  socket.emit('notification', 'Hello from the server!');
-};
-
-export default notificationSocket;
