68ffe6882fa07da51fc086e90996f8b96fb521da
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-12 00:59:19 +0700

    refactor(article-services): Transform ArticleService to an OOP structure

--- web/src/services/article/article-service.ts
+++ web/src/services/article/article-service.ts
@@ -1,9 +1,11 @@
 import api from '@/api';
 import { ApiResponse, ArticleType } from '@/types';
 
-export async function findArticle(slug: string): Promise<ArticleType> {
-  const response = await api.get<ApiResponse<ArticleType>>(
-    `/article/find/${slug}`
-  );
-  return response.data.data;
+export class articleServices {
+  static async findArticle(slug: string): Promise<ArticleType> {
+    const response = await api.get<ApiResponse<ArticleType>>(
+      `/article/find/${slug}`
+    );
+    return response.data.data;
+  }
 }
