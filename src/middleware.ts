bc5fbb4187446952816427c673b9b5cb72ae5674
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-30 04:05:12 +0700

    optimization middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,32 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-async function authenticated(req: NextRequest) {
-  const token = req.cookies.get('auth')?.value;
+// async function authenticated(req: NextRequest) {
+//   const token = req.cookies.get("auth")?.value;
 
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_BASE_URL_API + '/auth/verify',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/json',
-      },
-      body: JSON.stringify({ token }),
-    }
-  );
-  const data = await response.json();
-  return data.data;
-}
+//   const response = await fetch(
+//     process.env.NEXT_PUBLIC_BASE_URL_API + "/auth/verify",
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "Application/json",
+//       },
+//       body: JSON.stringify({ token }),
+//     },
+//   );
+//   const data = await response.json();
+//   return data.data;
+// }
 
 export async function middleware(req: NextRequest) {
-  const auth = await authenticated(req);
+  const auth = req.cookies.get("auth")?.value;
 
   if (!auth) {
-    return NextResponse.redirect(new URL('/', req.url));
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/article/new'],
+  matcher: ["/article/new"],
 };
