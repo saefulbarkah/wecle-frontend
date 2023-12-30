58c13c3c9451eb5dcd9c67b7e59481e307979902
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-30 11:48:45 +0700

    chore: Update API route rewrites in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -3,8 +3,16 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+        source: "/api/auth/login",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/auth/login",
+      },
+      {
+        source: "/api/auth/logout",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/auth/logout",
+      },
+      {
+        source: "/api/auth/register",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/auth/logout",
       },
     ];
   },
@@ -12,16 +20,16 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
