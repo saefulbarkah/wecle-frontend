007856d463fb96a9cedd1f42b805dd329e6d3b0a
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-31 14:34:29 +0700

    fix generate metadata on article

--- src/app/(universal)/article/[slug]/page.tsx
+++ src/app/(universal)/article/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import { Separator } from '@/components/ui/separator';
-import { Comments, ShowArticle } from '@/features/article/show';
-import { keywords } from '@/lib/meta-data';
-import { articleServices } from '@/services/article';
-import { ApiResponse, ArticleType } from '@/types';
-import type { Metadata } from 'next';
-import { redirect } from 'next/navigation';
+import { Separator } from "@/components/ui/separator";
+import { Comments, ShowArticle } from "@/features/article/show";
+import { keywords } from "@/lib/meta-data";
+import { articleServices } from "@/services/article";
+import { ApiResponse, ArticleType } from "@/types";
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 type Props = {
   params: { slug: string };
@@ -17,20 +17,21 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // fetch data
   try {
     const article: ApiResponse<ArticleType> = await fetch(
-      process.env.NEXT_PUBLIC_BASE_URL_API + '/article/find/' + slug
+      process.env.NEXT_PUBLIC_API_URL + "/article/find/" + slug,
+      {
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      },
     ).then((res) => res.json());
 
     return {
-      title: 'Article - ' + article.data.title,
+      title: "Article - " + article.data.title,
       description: article.data.title,
       keywords: [`${article.data.author.name}`, ...keywords],
     };
   } catch (error) {
-    return {
-      title: 'Article Not Found',
-      description: 'This article does not exist.',
-      keywords: keywords,
-    };
+    throw error;
   }
 }
 
@@ -45,10 +46,10 @@ async function getArticle(slug: string) {
 
 export default async function Page({ params }: Props) {
   const article = await getArticle(params.slug);
-  if (!article) return redirect('/');
+  if (!article) return redirect("/");
 
   return (
-    <div className="pt-[60px] mx-6 sm:max-w-2xl sm:mx-auto">
+    <div className="mx-6 pt-[60px] sm:mx-auto sm:max-w-2xl">
       <ShowArticle data={article} />
       <Separator />
       <Comments article={article} />
