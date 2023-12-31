5e92f282d92ab0f4fedce295312fb3cbd61c47e4
Saeful Barkah <saefulbarkah650@gmail.com>
2023-12-31 17:40:23 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const auth = req.cookies.get("auth")?.value;
 
-  // if (req.nextUrl.pathname.startsWith("/article/new")) {
-  //   if (!auth) {
-  //     return NextResponse.redirect(new URL("/", req.url));
-  //   }
-  // }
+  if (req.nextUrl.pathname.startsWith("/article/new")) {
+    if (!auth) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
 
   return NextResponse.next();
 }
