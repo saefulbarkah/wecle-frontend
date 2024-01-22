a53813b98e80a06a48c4e0cfbebd50cbf481561c
Saeful Barkah <saefulbarkah650@gmail.com>
2024-01-22 20:38:37 +0700

    optimized space floating label input

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -36,7 +36,7 @@ const Text = React.forwardRef<HTMLInputElement, inputProps>(
       <div className={`relative ${error ? "my-2" : ""}`}>
         <input
           className={cn(
-            `peer w-full appearance-none bg-transparent p-1 placeholder-transparent outline-none transition selection:bg-yellow-50  placeholder:pointer-events-none placeholder:select-none autofill:bg-transparent focus:border-primary motion-reduce:transition-none dark:border-white/20 dark:focus:border-primary`,
+            `peer w-full appearance-none bg-transparent p-1  placeholder-transparent outline-none transition selection:bg-yellow-50  placeholder:pointer-events-none placeholder:select-none autofill:bg-transparent focus:border-primary motion-reduce:transition-none dark:border-white/20 dark:focus:border-primary`,
             className,
           )}
           ref={ref}
@@ -48,7 +48,7 @@ const Text = React.forwardRef<HTMLInputElement, inputProps>(
             error ? "bg-danger peer-focus:bg-danger" : ""
           }`}
         ></div>
-        <label className="pointer-events-none absolute left-[1px] -translate-y-[1.1rem] text-xs transition-[transform,font] !duration-150 peer-placeholder-shown:-translate-y-1 peer-placeholder-shown:text-sm peer-focus:-translate-y-[1.1rem] peer-focus:text-xs peer-focus:font-normal peer-focus:text-black motion-reduce:transition-none dark:peer-focus:text-white">
+        <label className="pointer-events-none absolute left-0 -translate-y-full text-xs transition-[transform,font] !duration-150 peer-placeholder-shown:-translate-y-1 peer-placeholder-shown:text-sm peer-focus:-translate-y-full peer-focus:text-xs peer-focus:font-normal peer-focus:text-black motion-reduce:transition-none dark:peer-focus:text-white">
           {placeholder}
         </label>
         {error && (
